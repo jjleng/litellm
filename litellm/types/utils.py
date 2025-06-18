8a465ca1c1475a9aa10b7f314870c3616f4e12d1
Krrish Dholakia <krrishdholakia@gmail.com>
2025-06-17 18:51:40 -0700

    fix(types/utils.py): fix check

--- litellm/types/utils.py
+++ litellm/types/utils.py
@@ -1130,7 +1130,6 @@ class ModelResponseBase(OpenAIObject):
 class ModelResponseStream(ModelResponseBase):
     choices: List[StreamingChoices]
     provider_specific_fields: Optional[Dict[str, Any]] = Field(default=None)
-    usage: Optional[Usage] = Field(default=None)
 
     def __init__(
         self,
@@ -1142,6 +1141,7 @@ class ModelResponseStream(ModelResponseBase):
         provider_specific_fields: Optional[Dict[str, Any]] = None,
         **kwargs,
     ):
+        usage: Optional[Usage] = None
         if choices is not None and isinstance(choices, list):
             new_choices = []
             for choice in choices:
@@ -1171,7 +1171,12 @@ class ModelResponseStream(ModelResponseBase):
             and kwargs["usage"] is not None
             and isinstance(kwargs["usage"], dict)
         ):
-            kwargs["usage"] = Usage(**kwargs["usage"])
+            # remove usage from kwargs
+            usage = kwargs["usage"]
+            if isinstance(usage, dict):
+                usage = Usage(**usage)
+            elif isinstance(usage, Usage):
+                usage = usage
 
         kwargs["id"] = id
         kwargs["created"] = created
@@ -1179,6 +1184,9 @@ class ModelResponseStream(ModelResponseBase):
         kwargs["provider_specific_fields"] = provider_specific_fields
 
         super().__init__(**kwargs)
+
+        if usage is not None:
+            self.usage = usage
 
     def __contains__(self, key):
         # Define custom behavior for the 'in' operator
